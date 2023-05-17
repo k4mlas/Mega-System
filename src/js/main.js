bb8f9831344920fc36cff16d68d9b3dbdc39faff
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-05-17 19:18:27 +0200

    working navMobile and navDesktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 const btnNav = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.header__img__navMobile');
+const navLinks = document.querySelectorAll('.header__img__navMobile__link');
 
 const showNav = () => {
 	btnNav.classList.toggle('is-active');
 	navMobile.classList.toggle('header__img__navMobile--active');
+	navLinks.forEach((item) => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('header__img__navMobile--active');
+			btnNav.classList.remove('is-active');
+		});
+	});
 };
 
 btnNav.addEventListener('click', showNav);

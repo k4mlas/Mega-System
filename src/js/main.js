13567e035464c250c260e4dd50eaed9f738301fe
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-05-21 14:17:07 +0200

    i add background color for dekstop

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 let $btnNavMobile;
 let $navMobile;
 let $navLinksMobile;
+let $navDesktop;
 ///////////////////////////////////////
 
 const main = () => {
@@ -13,11 +14,16 @@ const prepareDOMElements = () => {
 	//zmienne do nawigacji mobilnej
 	$btnNavMobile = document.querySelector('.hamburger');
 	$navMobile = document.querySelector('.header__img__navMobile');
-	$navLinksMobile = document.querySelectorAll('.header__img__navMobile__body__link');
+	$navLinksMobile = document.querySelectorAll(
+		'.header__img__navMobile__body__link'
+	);
+	//zmienne do nawigacji desktop
+	$navDesktop = document.querySelector('.header__img__navDesktop__navigaction');
 };
 
 const prepareDOMEvens = () => {
 	$btnNavMobile.addEventListener('click', showNav);
+	window.addEventListener('scroll', addBacground);
 };
 ///////////////////////////////////////////////////////Funkcje
 
@@ -29,8 +35,17 @@ const showNav = () => {
 		item.addEventListener('click', () => {
 			$navMobile.classList.remove('header__img__navMobile--active');
 			$btnNavMobile.classList.remove('is-active');
+			document.body.classList.remove('sticky-body');
 		});
 	});
+};
+
+const addBacground = () => {
+	if (window.scrollY >= 250) {
+		$navDesktop.classList.add('addBackground');
+	} else {
+		$navDesktop.classList.remove('addBackground');
+	}
 };
 
 ////////////////////////////////////////////////////////Funkcja Main

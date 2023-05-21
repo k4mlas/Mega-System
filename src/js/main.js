9359b1cffa43acebe3a78a446dca96a3b3cb6844
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-05-21 12:30:31 +0200

    RWD

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,38 @@
-const btnNav = document.querySelector('.hamburger');
-const navMobile = document.querySelector('.header__img__navMobile');
-const navLinks = document.querySelectorAll('.header__img__navMobile__link');
+////////////////////////////////////////zmienne do strony
+let $btnNavMobile;
+let $navMobile;
+let $navLinksMobile;
+///////////////////////////////////////
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvens();
+};
+
+const prepareDOMElements = () => {
+	//zmienne do nawigacji mobilnej
+	$btnNavMobile = document.querySelector('.hamburger');
+	$navMobile = document.querySelector('.header__img__navMobile');
+	$navLinksMobile = document.querySelectorAll('.header__img__navMobile__body__link');
+};
+
+const prepareDOMEvens = () => {
+	$btnNavMobile.addEventListener('click', showNav);
+};
+///////////////////////////////////////////////////////Funkcje
 
 const showNav = () => {
-	btnNav.classList.toggle('is-active');
-	navMobile.classList.toggle('header__img__navMobile--active');
-	navLinks.forEach((item) => {
+	$btnNavMobile.classList.toggle('is-active');
+	$navMobile.classList.toggle('header__img__navMobile--active');
+	document.body.classList.toggle('sticky-body');
+	$navLinksMobile.forEach((item) => {
 		item.addEventListener('click', () => {
-			navMobile.classList.remove('header__img__navMobile--active');
-			btnNav.classList.remove('is-active');
+			$navMobile.classList.remove('header__img__navMobile--active');
+			$btnNavMobile.classList.remove('is-active');
 		});
 	});
 };
 
-btnNav.addEventListener('click', showNav);
+////////////////////////////////////////////////////////Funkcja Main
+
+document.addEventListener('DOMContentLoaded', main);

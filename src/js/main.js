384f46cc225897cb92a309f76d7bc30a15c56ebe
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-06-18 10:08:01 +0200

    finish popup

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,17 @@ let $navMobile;
 let $navLinksMobile;
 let $navDesktop;
 let $footerYear;
+
+let $name;
+let $email;
+let $text;
+let $formBtn;
+let $infoForm;
+let $nameError;
+let $emailError;
+let $textError;
+let $popup;
+let $popupBtn;
 ///////////////////////////////////////
 
 const main = () => {
@@ -23,17 +34,29 @@ const prepareDOMElements = () => {
 	$navDesktop = document.querySelector('.header__img__navDesktop__navigaction');
 	// zmienna do roku w footer
 	$footerYear = document.querySelector('.footerYear');
+	//zmienne do formularza
+	$name = document.querySelector('#name');
+	$email = document.querySelector('#mail');
+	$text = document.querySelector('#msg');
+	$formBtn = document.querySelector('.contact__form__body__btn');
+	$infoForm - document.querySelector('.popup');
+	$nameError = document.querySelector('#nameError');
+	$emailError = document.querySelector('#emailError');
+	$textError = document.querySelector('#textError');
+	$popup = document.querySelector('.popup');
+	$popupBtn = document.querySelector('.popup__btn');
 };
 
 const prepareDOMEvens = () => {
 	$btnNavMobile.addEventListener('click', showNav);
 	window.addEventListener('scroll', addBacground);
+	$formBtn.addEventListener('click', formConact);
+	$popupBtn.addEventListener('click', closePopup);
 };
 ///////////////////////////////////////////////////////Funkcje
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
-	console.log(year);
 	$footerYear.innerText = year;
 };
 
@@ -50,7 +73,6 @@ const showNav = () => {
 	});
 };
 
-
 const addBacground = () => {
 	if (window.scrollY >= 250) {
 		$navDesktop.classList.add('addBackground');
@@ -59,6 +81,45 @@ const addBacground = () => {
 	}
 };
 
+const formConact = () => {
+	if ($text.value != '' && $name.value != '' && $email.value != '') {
+		showpop();
+		$text.value = '';
+		$email.value = '';
+		$name.value = '';
+		$nameError.style.visibility = 'hidden';
+		$emailError.style.visibility = 'hidden';
+		$textError.style.visibility = 'hidden';
+	} else {
+		checkForm();
+	}
+};
+
+const checkForm = () => {
+	if ($name.value == '') {
+		$nameError.style.visibility = 'visible';
+	} else {
+		$nameError.style.visibility = 'hidden';
+	}
+	if ($email.value == '') {
+		$emailError.style.visibility = 'visible';
+	} else {
+		$emailError.style.visibility = 'hidden';
+	}
+	if ($text.value == '') {
+		$textError.style.visibility = 'visible';
+	} else {
+		$textError.style.visibility = 'hidden';
+	}
+};
+
+const showpop = () => {
+	$popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+	$popup.style.display = 'none';
+};
 ////////////////////////////////////////////////////////Funkcja Main
 
 document.addEventListener('DOMContentLoaded', main);
